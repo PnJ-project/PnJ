c797813e1be3803f3079ac7c4705cc40f95f5f69
임대영 <ldy092000@gmail.com>
2023-10-26 15:54:59 +0900

    refact/be/login-api: mapping uri, log 설정 수정

--- backend/src/main/java/com/preparedhypeboys/pnj/domain/member/api/LoginController.java
+++ backend/src/main/java/com/preparedhypeboys/pnj/domain/member/api/LoginController.java
@@ -8,9 +8,9 @@ import com.preparedhypeboys.pnj.domain.member.dto.MemberResponseDto.LoginReponse
 import com.preparedhypeboys.pnj.domain.member.service.OAuth2UserService;
 import com.preparedhypeboys.pnj.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/login")
-@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("api/login")
+@Slf4j
 public class LoginController {
 
     private final OAuth2UserService oAuth2UserService;
@@ -37,6 +37,8 @@ public class LoginController {
     // TODO : oauth 콜백 함수 작성
     @GetMapping("/oauth2/code/google")
     public ResponseEntity<ResponseDto<String>> callback() {
+
+        log.info("콜백 완료");
         return null;
     }
 }
