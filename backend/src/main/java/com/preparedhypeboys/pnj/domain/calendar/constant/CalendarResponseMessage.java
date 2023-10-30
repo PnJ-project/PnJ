f9c0386bb1a050d4aa89d16620e1b690a3129783
임대영 <ldy092000@gmail.com>
2023-10-30 14:06:31 +0900

    feat/be/message: 이벤트 삽입 성공 메세지 추가

--- backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/constant/CalendarResponseMessage.java
+++ backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/constant/CalendarResponseMessage.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CalendarResponseMessage {
-    GET_EVENT_SUCCESS("이벤트 리스트 조회 완료");
+    GET_EVENT_SUCCESS("이벤트 리스트 조회 완료"),
+    INSERT_EVENT_SUCCESS("이벤트 직접 생성 완료");
 
     private final String message;
 }
