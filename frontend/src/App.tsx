d045ebb29d471356d9580365d06dc844d15a96f1
조준하 <whwnsgk830@naver.com>
2023-10-30 11:02:16 +0900

    feat/fe : react환경으로 리펙토링

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,43 @@
 // import React from 'react';
 import { Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
 import Calendar from "./pages/Calendar";
 import Community from "./pages/Community";
-import Main from "./pages/Main";
 import Login from "./pages/Login";
 import MyPage from "./pages/MyPage";
 import SignUp from "./pages/SignUp";
 import Solution from "./pages/Solution";
 import SolutionList from "./pages/SolutionList";
+import Notfound from "./pages/service/404";
+import About from "./pages/service/About";
+import Contact from "./pages/service/Contact";
+import Team from "./pages/service/Team";
+import PageManager from "./pages/PageManager";
+import TestManager from "./pages/test/TestManager";
 export default function App() {
-  
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/calendar" element={<Calendar />} />
-      <Route path="/community" element={<Community />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/solution" element={<Solution />} />
-      <Route path="/solutionlist" element={<SolutionList />} />
-    </Routes>
+      <Routes>
+        {/* 홈 */}
+        <Route path="/" element={<Main />} />
+        {/* 메인 서비스 */}
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/solution" element={<Solution />} />
+        <Route path="/solutionlist" element={<SolutionList />} />
+        {/* 회원관리 */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<MyPage />} />
+        {/* 기타 */}
+        <Route path="*" element={<Notfound />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/team" element={<Team />} />
+        {/* 개발자용 */}
+        <Route path="/pm" element={<PageManager />} />
+        <Route path="/tm" element={<TestManager />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  
-  <React.StrictMode>
+const clientId =
+  "303398331485-bi8j2vvltn2lk7ah62dhuo2qpgbqmp1j.apps.googleusercontent.com";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  // <React.StrictMode>
+  <GoogleOAuthProvider clientId={clientId}>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
-
+  </GoogleOAuthProvider>
+  // </React.StrictMode>
+);
