235db4bd7672b04896e192024c0dfc3af1068c44
임대영 <ldy092000@gmail.com>
2023-10-30 14:51:12 +0900

    feat/be : api - 이벤트 삭제 api

--- backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/api/CalendarController.java
+++ backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/api/CalendarController.java
@@ -1,5 +1,6 @@
 package com.preparedhypeboys.pnj.domain.calendar.api;
 
+import static com.preparedhypeboys.pnj.domain.calendar.constant.CalendarResponseMessage.DELETE_EVENT_SUCCESS;
 import static com.preparedhypeboys.pnj.domain.calendar.constant.CalendarResponseMessage.GET_EVENT_SUCCESS;
 import static com.preparedhypeboys.pnj.domain.calendar.constant.CalendarResponseMessage.INSERT_EVENT_SUCCESS;
 
@@ -11,6 +12,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +46,18 @@ public class CalendarController {
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDto.create(INSERT_EVENT_SUCCESS.getMessage(),
                 calendarService.createEvent(requestDto))
+        );
+    }
+
+    @DeleteMapping(value = "/{memberId}/{eventId}")
+    public ResponseEntity<ResponseDto<?>> deleteEvent(
+        @PathVariable(value = "memberId") Long memberId,
+        @PathVariable(value = "eventId") String eventId
+    ) {
+        calendarService.deleteEvent(memberId, eventId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+            ResponseDto.create(DELETE_EVENT_SUCCESS.getMessage())
         );
     }
 
