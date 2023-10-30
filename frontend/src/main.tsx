246cb9a7e6318843d9a1cf8332ff3a6d86cae4c0
조준하 <whwnsgk830@naver.com>
2023-10-30 12:30:09 +0900

    feat/fe : 기본 css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./font/font.css";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -2,11 +2,9 @@
 // import React from "react";
 import plogo from "../assets/main.svg";
 import UseDemo from "../components/atoms/UseDemo";
-// import GoogleLogin from "../components/atoms/GoogleLogin";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
+import GoogleLogin from "../components/atoms/GoogleLogin";
 import styled from "styled-components";
 export default function Main() {
-
   // const clientId: string = process.env.GOOGLE_CLIENT_ID!;
 
   return (
@@ -14,15 +12,15 @@ export default function Main() {
       <Container>
         <TextDiv>
           <Title>
-            <div><Span>음성과 텍스트</Span>로</div>
+            <div>
+              <Span>음성과 텍스트</Span>로
+            </div>
             혁신적인 일정관리를 경험해보세요
           </Title>
           <Content>innovatory Experience schedule management</Content>
           <Btns>
             <UseDemo />
-            {/* <GoogleOAuthProvider clientId={clientId}>
-              <GoogleLogin />
-            </GoogleOAuthProvider> */}
+            <GoogleLogin />
           </Btns>
         </TextDiv>
         <LogoImg src={plogo} alt="PnJ LOGO" />
@@ -34,7 +32,8 @@ export default function Main() {
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
-`
+  height: 90vh;
+`;
 const TextDiv = styled.div`
   display: flex;
   flex-direction: column;
@@ -42,18 +41,17 @@ const TextDiv = styled.div`
   gap: 16px;
   margin-top: auto;
   padding-left: 40px;
-`
+`;
 const Title = styled.div`
   font-family: TheJamsil5;
   font-size: 50px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
-`
+`;
 const Span = styled.span`
-  color: #00900E;
-  
-`
+  color: #00900e;
+`;
 const Content = styled.div`
   color: #000;
   font-family: SUITE-Regular;
@@ -61,17 +59,16 @@ const Content = styled.div`
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-`
+`;
 const Btns = styled.div`
   display: flex;
   flex-direction: column;
   width: 295px;
   gap: 10px;
-`
+`;
 const LogoImg = styled.img`
   padding-right: 40px;
-  
-`
+`;
 // SSR - 초기 데이터 불러오기
 /*
 export async function getServerSideProps() {
