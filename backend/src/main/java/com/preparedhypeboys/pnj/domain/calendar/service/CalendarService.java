f483417c70c91966fba6bf87a879f56b1e80296d
임대영 <ldy092000@gmail.com>
2023-10-30 15:44:55 +0900

    refact/be : service - dto 인자 변경

--- backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/service/CalendarService.java
+++ backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/service/CalendarService.java
@@ -1,20 +1,19 @@
 package com.preparedhypeboys.pnj.domain.calendar.service;
 
-import com.preparedhypeboys.pnj.domain.calendar.dto.CalendarRequestDto.InsertEventRequestDto;
+import com.preparedhypeboys.pnj.domain.calendar.dto.CalendarRequestDto.EventRequestDto;
 import com.preparedhypeboys.pnj.domain.calendar.dto.EventDto;
 import java.util.List;
-import org.springframework.transaction.annotation.Transactional;
 
 
 public interface CalendarService {
 
     public List<EventDto> readEventList(Long memberId, String tiemMax, String timeMin);
 
-    public EventDto createEvent(InsertEventRequestDto event);
+    public EventDto createEvent(EventRequestDto event);
 
     public void deleteEvent(Long memberId, String eventId);
 
-    public EventDto updateEvent(Long memberId, EventDto event);
+    public EventDto updateEvent(EventRequestDto requestDto);
 
     public List<EventDto> updateEventList(EventDto event);
 
