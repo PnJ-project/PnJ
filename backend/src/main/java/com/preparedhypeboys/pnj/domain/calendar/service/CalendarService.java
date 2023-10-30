8589ee952b2d305a085f9c1f1c2b8f1c7639871d
임대영 <ldy092000@gmail.com>
2023-10-30 14:06:13 +0900

    feat/be/service: 구글 캘린더 이벤트 삽입 서비스 로직 작성

--- backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/service/CalendarService.java
+++ backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/service/CalendarService.java
@@ -1,5 +1,6 @@
 package com.preparedhypeboys.pnj.domain.calendar.service;
 
+import com.preparedhypeboys.pnj.domain.calendar.dto.CalendarRequestDto.InsertEventRequestDto;
 import com.preparedhypeboys.pnj.domain.calendar.dto.EventDto;
 import java.util.List;
 
@@ -8,7 +9,7 @@ public interface CalendarService {
 
     public List<EventDto> readEventList(Long memberId, String tiemMax, String timeMin);
 
-    public EventDto createEvent(EventDto event);
+    public EventDto createEvent(InsertEventRequestDto event);
 
     public void deleteEvent(String eventId);
 
--- backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/service/CalendarServiceImpl.java
+++ backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/service/CalendarServiceImpl.java
@@ -1,6 +1,7 @@
 package com.preparedhypeboys.pnj.domain.calendar.service;
 
 import com.preparedhypeboys.pnj.domain.calendar.dao.GoogleCalendarDao;
+import com.preparedhypeboys.pnj.domain.calendar.dto.CalendarRequestDto.InsertEventRequestDto;
 import com.preparedhypeboys.pnj.domain.calendar.dto.EventDto;
 import com.preparedhypeboys.pnj.domain.member.dao.MemberRepository;
 import com.preparedhypeboys.pnj.domain.member.entity.Member;
@@ -22,13 +23,21 @@ public class CalendarServiceImpl implements
     @Override
     public List<EventDto> readEventList(Long memberId, String tiemMax, String timeMin) {
         Optional<Member> member = memberRepository.findById(memberId);
-        return googleCalendarDao.getEventList(tiemMax, timeMin, member.get().getAccessToken());
+
+        // TODO 예외처리
+        return member.map(
+                value -> googleCalendarDao.getEventList(tiemMax, timeMin, value.getAccessToken()))
+            .orElse(null);
     }
 
     @Override
     @Transactional
-    public EventDto createEvent(EventDto event) {
-        return null;
+    public EventDto createEvent(InsertEventRequestDto requestDto) {
+        Optional<Member> member = memberRepository.findById(requestDto.getMemberId());
+
+        // TODO 예외처리
+        return member.map(value -> googleCalendarDao.insertEvent(requestDto.getEventDto(),
+            value.getAccessToken())).orElse(null);
     }
 
     @Override
