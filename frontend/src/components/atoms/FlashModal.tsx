773cc2261f110ac6d8d64235e550de3c9e52f2c8
조준하 <whwnsgk830@naver.com>
2023-10-31 17:25:15 +0900

    feat/fe : 기본 atom 생성

---
+++ frontend/src/components/atoms/FlashModal.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFlashModalFalse } from "../../store/slice/ToggleSlice";
+import { RootState } from "../../store/store";
+import "./FlashModal.css";
+
+export default function NoLandMessage() {
+  // 기본세팅
+  const dispatch = useDispatch();
+  const isModalMsgActive = useSelector(
+    (state: RootState) => state.toggle.isUseDemo
+  );
+  const modalMsg = useSelector(
+    (state: RootState) => state.string.flashModalMdg
+  );
+
+  useEffect(() => {
+    // 선택 가능한 땅이 없을 때, 2초 동안 메시지를 보여준 후 숨김
+    const timer = setTimeout(() => {
+      dispatch(setFlashModalFalse());
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isModalMsgActive]);
+
+  return isModalMsgActive ? (
+    <div className="noLandMessage">{modalMsg}</div>
+  ) : null;
+}
