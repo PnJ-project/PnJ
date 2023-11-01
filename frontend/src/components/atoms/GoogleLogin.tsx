74d7b6ce15bdafabcbc9c6e504304a21c47cbe81
박승희 <pshee0329@naver.com>
2023-11-02 07:43:17 +0900

    feat/fe : 로그인 파일 api 수정, CalendarSlice 생성

--- frontend/src/components/atoms/GoogleLogin.tsx
+++ frontend/src/components/atoms/GoogleLogin.tsx
@@ -1,21 +1,28 @@
-// import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useGoogleLogin } from "@react-oauth/google";
 import "./Google.css";
 import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import { setUserData } from '../../store/slice/AuthSlice'
 
 export default function GoogleLogin() {
-  const backend = import.meta.env.VITE_APP_BACKEND_SERVER;
+  // 기본 세팅
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const backend = import.meta.env.VITE_APP_BACKEND_URL;
   const googleSocialLogin = useGoogleLogin({
     // scope : 환경 변수 파일로 받아오는게 좋을 것 같습니다
     scope: "email profile https://www.googleapis.com/auth/calendar",
     onSuccess: async ({ code }) => {
       // 로그인 완료 시 액세스 토큰 받기 위한 code
       console.log(code);
-      const tokens = await axios.get(
+      const response = await axios.get(
         `http://${backend}:8080/api/login` + "?code=" + `${code}`
       );
       // 미작성(현재 액)
-      console.log(tokens.data);
+      console.log(response.data);
+      dispatch(setUserData(response.data.data));
+      navigate('/')
     },
     onError: (errorResponse) => {
       console.error(errorResponse);
--- frontend/src/store/store.tsx
+++ frontend/src/store/store.tsx
@@ -4,12 +4,14 @@ import counterReducer from "./slice/CounterSlice";
 import stringReducer from "./slice/StringSlice";
 import listReducer from "./slice/ListSlice";
 import authReducer from "./slice/AuthSlice";
+import calendarReducer from './slice/CalendarSlice';
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   string: stringReducer,
   list: listReducer,
   auth: authReducer,
+  calendar: calendarReducer,
 });
 
 const store = configureStore({
