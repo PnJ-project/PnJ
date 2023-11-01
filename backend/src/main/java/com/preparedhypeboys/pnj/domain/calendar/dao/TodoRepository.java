d93b9fc7bcaad29147696bb5e828cd41cfeb43a8
임대영 <ldy092000@gmail.com>
2023-11-01 17:04:32 +0900

    feat/be : Todo Dao - 반환 dto 변경

--- backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/dao/TodoRepository.java
+++ backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/dao/TodoRepository.java
@@ -1,12 +1,12 @@
 package com.preparedhypeboys.pnj.domain.calendar.dao;
 
-import com.preparedhypeboys.pnj.domain.calendar.dto.TodoResponseDto.TodoDto;
+import com.preparedhypeboys.pnj.domain.calendar.dto.TodoResponseDto;
 import com.preparedhypeboys.pnj.domain.calendar.entity.Todo;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    List<TodoDto> findTodosByMemberId(Long memberId);
+    List<TodoResponseDto> findTodosByMemberId(Long memberId);
 
 }
