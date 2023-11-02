e8b5ee0ea669b96af337dafd348a1b257a38a493
임대영 <ldy092000@gmail.com>
2023-11-02 10:38:47 +0900

    feat/be : Message - Event -> Todo

--- backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/constant/CalendarResponseMessage.java
+++ backend/src/main/java/com/preparedhypeboys/pnj/domain/calendar/constant/CalendarResponseMessage.java
@@ -9,7 +9,8 @@ public enum CalendarResponseMessage {
     GET_EVENT_SUCCESS("이벤트 리스트 조회 완료"),
     INSERT_EVENT_SUCCESS("이벤트 직접 생성 완료"),
     DELETE_EVENT_SUCCESS("이벤트 삭제 완료"),
-    UPDATE_EVENT_SUCCESS("이벤트 수정 완료");
+    UPDATE_EVENT_SUCCESS("이벤트 수정 완료"),
+    EXCHANGE_TO_TODO_SUCCESS("이벤트 -> TODO 변환 완료");
 
     private final String message;
 }
