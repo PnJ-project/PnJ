6bfb0889f6f685ece7ebe59ffaa10a6d0ba4f580
조재웅 <jaewoongcho93@gmail.com>
2023-11-03 13:34:09 +0900

    fix/be : build error, exclude empty test

--- backend/src/test/java/com/preparedhypeboys/pnj/PnjApplicationTests.java
+++ backend/src/test/java/com/preparedhypeboys/pnj/PnjApplicationTests.java
@@ -3,7 +3,6 @@ package com.preparedhypeboys.pnj;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PnjApplicationTests {
 
 	@Test
