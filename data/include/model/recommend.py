37aa30407b147f8ebcb229bf62c97177c5762596
김혜진 <zlagg23@gmail.com>
2023-11-09 15:20:27 +0900

    feat : 공연 데이터 추가

--- data/include/model/recommend.py
+++ data/include/model/recommend.py
@@ -1,6 +1,4 @@
 import json
-import pandas as pd
-import numpy as np
 from konlpy.tag import Okt
 from include.dataloader.dataloader import load_data
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -13,20 +11,17 @@ def get_morphemes(text):
     morphemes = okt.morphs(text)
     return morphemes
 
-
 def data_preprocessing():
-
     data_load = load_data('place_01.csv')
-    selected_data = data_load[['title', 'info', 'category', 'location']]
+
+    selected_data = data_load[['title', 'info', 'category', 'category_name']]
 
     return data_load, selected_data
 
-def calcultation_similarity():
+def calcultation_similarity(summary_list):
     result = []
 
     origin_data, selected_data = data_preprocessing()
-
-    target_data = ['떡볶이 쇼핑 여행 강릉 여행 제주도 여행']
 
     # TF-IDF Vectorizer 초기화
     tfidf_vectorizer = TfidfVectorizer()
@@ -35,7 +30,7 @@ def calcultation_similarity():
     tfidf_matrix = tfidf_vectorizer.fit_transform(selected_data.apply(lambda x: ' '.join(x), axis=1))
 
     # 타겟 데이터 벡터화
-    target_matrix = tfidf_vectorizer.transform(target_data)
+    target_matrix = tfidf_vectorizer.transform(summary_list)
 
     # 코사인 유사도 계산
     cosine_similarities = cosine_similarity(target_matrix, tfidf_matrix)
@@ -45,34 +40,15 @@ def calcultation_similarity():
     top_similarities = sorted(similarities_with_target, key=lambda x: x[1], reverse=True)[:20]
 
     for idx, similarity in top_similarities:
+        # print("index",idx)
         result.append(origin_data.iloc[idx].tolist())
         print(f"유사도: {similarity}, 데이터: {selected_data.iloc[idx]}")
 
-    keys = ["category", "title", "info", "road_address", "jibun_address", "location", "homepage", "image"]
+    keys = ["category", "title", "info", "road_address", "jibun_address", "category_name", "homepage", "image"]
     json_data = [dict(zip(keys, item)) for item in result]
 
     json_result = json.dumps(json_data, ensure_ascii=False, indent=4)
 
-
     return json_result
 
-
-
-def test():
-    list1 = ['꽃보라동산', '기념물', '여행', '서울특별시 용산구']
-    list2 = ['꽃보라동산', '여행', '강릉여행', '전라남도 기념물']
-
-    # TF-IDF Vectorizer 초기화
-    tfidf_vectorizer = TfidfVectorizer()
-
-    # 두 리스트를 합쳐서 하나의 문서로 만듦
-    combined_lists = [' '.join(list1), ' '.join(list2)]
-    print(combined_lists)
-    # TF-IDF 행렬 계산
-    tfidf_matrix = tfidf_vectorizer.fit_transform(combined_lists)
-
-    # 두 벡터 간의 코사인 유사도 계산
-    cosine_similarities = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1])
-
-    print("유사도", cosine_similarities)
 
