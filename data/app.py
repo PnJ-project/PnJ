c4d283ecaf899c10f9a0d1dcbd03e9d47db874ce
김혜진 <zlagg23@gmail.com>
2023-11-10 13:58:15 +0900

    Update app.py

--- data/app.py
+++ data/app.py
@@ -27,7 +27,7 @@ def get_recommend():
     return result
 
 
-@app.route('/')
+@app.route(PREFIX +'/')
 def hello_world():  # put application's code here
     print('Hello Console!')
     return 'Hello World!'
