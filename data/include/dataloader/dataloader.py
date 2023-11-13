12cb4023173cf6cd13c166cadec985ed087dfdad
김혜진 <zlagg23@gmail.com>
2023-11-13 13:50:47 +0900

    feat : 추천(여행지, 스포츠, 공부, 공연/전시, 맛집)

--- data/include/dataloader/dataloader.py
+++ data/include/dataloader/dataloader.py
@@ -7,11 +7,18 @@ def load_data(filename):
 
 
 def data_preprocessing():
-    file_paths = ['place.csv', 'show.csv', 'sports.csv']
+    # 공부 카테고리는 따로 csv로드
+    life_quotes = load_data('life_quotes.csv')
+    file_paths = ['place.csv', 'show.csv', 'sports.csv', 'restaurant.csv']
     data_frames = [load_data(file_path) for file_path in file_paths]
 
     merged_data = pd.concat(data_frames, ignore_index=True)
     # 데이터 병합
-    selected_data = merged_data[['title', 'info', 'category', 'category_name']]
+    selected_data = merged_data[['title', 'info', 'category', 'categoryName']]
 
-    return merged_data, selected_data
+    new_row = pd.DataFrame({'title': ['명언'], 'info': ['시험, 면접, 테스트, 자격증'], 'category': ['공부'],
+                            'categoryName': ['명상, 요가, 자기계발']})
+
+    selected_data = pd.concat([selected_data, new_row], ignore_index=True)
+
+    return merged_data, selected_data, life_quotes
