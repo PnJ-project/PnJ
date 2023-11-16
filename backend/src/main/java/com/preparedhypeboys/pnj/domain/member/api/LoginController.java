12d3d982c580a34e35f1b32ebed9346c42d8fc30
임대영 <ldy092000@gmail.com>
2023-11-16 14:47:03 +0900

    feat/be : api - authenticationPrincipal 적용

--- backend/src/main/java/com/preparedhypeboys/pnj/domain/member/api/LoginController.java
+++ backend/src/main/java/com/preparedhypeboys/pnj/domain/member/api/LoginController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PreAuthorize("hasAuthority('refreshToken')")
 @RequiredArgsConstructor
 @RequestMapping("/api/login")
 @Slf4j
