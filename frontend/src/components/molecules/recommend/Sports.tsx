f24c2e4ae5450fb4ad51b48de96ab7ba0f5234bd
조준하 <whwnsgk830@naver.com>
2023-11-17 18:49:01 +0900

    feat/fe : 추천 카테고리 캘린더 및 투두 등록

--- frontend/src/components/molecules/recommend/Sports.tsx
+++ frontend/src/components/molecules/recommend/Sports.tsx
@@ -10,10 +10,19 @@ import {
 import { useState, useEffect } from "react";
 import SportsRecommend from "./SportsRecommend";
 import styled from "styled-components";
+import moment from "moment";
+import { useQuery } from "react-query";
+import { readCalendar } from "../../../api/CalendarApi";
+import axiosInstance from "../../../functions/AxiosInstance";
+import { setAuthorizationHeaderInter } from "../../../functions/BaseFunc";
 
 export default function Sports() {
   // 기본 세팅
   const recommends = useSelector(selectRecommends);
+  const [categoryHover, setCategoryHover] = useState({
+    boolean: false,
+    index: -1,
+  });
   const [items, setItems] = useState<
     | TripItemType[]
     | StudyItemType[]
@@ -21,6 +30,66 @@ export default function Sports() {
     | MusicItemType[]
     | SportItemType[]
   >([]);
+  const local_back_url = import.meta.env.VITE_APP_BACKEND_SERVER_LIVE;
+  const [memberId] = useState(Number(localStorage.getItem("memberId")));
+  const startOfFiveMonthsAgo = moment()
+    .subtract(6, "months")
+    .startOf("month")
+    .toDate()
+    .toISOString(); // 5개월 전
+  const endOfFiveMonthsAhead = moment()
+    .add(6, "months")
+    .endOf("month")
+    .endOf("week")
+    .toDate()
+    .toISOString(); // 5개월 후
+  const [timeMax] = useState(startOfFiveMonthsAgo);
+  const [timeMin] = useState(endOfFiveMonthsAhead);
+  // 쿼리세팅
+  const { refetch: refetchCal } = useQuery(
+    "calendarData",
+    () => readCalendar(timeMax, timeMin),
+    { retry: false }
+  ); // calendar API
+
+  // 캘린더 추가
+  const handleAddEvent = async (id: number) => {
+    const reqNewEvent = {
+      memberId: memberId,
+      event: {
+        id: null,
+        summary: `(${items[id].categoryName}) ${items[id].awayTeamName} vs ${items[id].homeTeamName}`,
+        colorId: null,
+        start: {
+          dateTime: moment(items[id].gameDateTime, "YYYY.MM.DD")
+            .toDate()
+            .toISOString(),
+          timeZone: "Asia/Seoul",
+          date: null,
+        },
+        end: {
+          dateTime: moment(items[id].gameDateTime, "YYYY.MM.DD")
+            .toDate()
+            .toISOString(),
+          timeZone: "Asia/Seoul",
+          date: null,
+        },
+      },
+    };
+    await setAuthorizationHeaderInter();
+    try {
+      const response = await axiosInstance.post(
+        `${local_back_url}/api/calendar/v2`,
+        reqNewEvent
+      );
+      // 캘린더 다시 불러오기
+      console.log("구글 캘린더 생성 완료", response);
+      await refetchCal();
+    } catch (error) {
+      console.error("구글 캘린더 생성 에러:", error);
+      return;
+    }
+  };
 
   // 정보 추리기
   useEffect(() => {
@@ -52,7 +121,32 @@ export default function Sports() {
               <SportsRecommend>
                 {items.map((item, index) => (
                   <SliderItem key={index}>
-                    <ImageContainer>
+                    <ImageContainer
+                      onMouseEnter={() =>
+                        setCategoryHover({
+                          boolean: true,
+                          index: index,
+                        })
+                      }
+                      onMouseLeave={() =>
+                        setCategoryHover({
+                          boolean: false,
+                          index: -1,
+                        })
+                      }
+                    >
+                      <button
+                        className={`CategoryAdd ${
+                          categoryHover.boolean && categoryHover.index == index
+                            ? "CategoryAddActive"
+                            : ""
+                        }`}
+                        onClick={() => {
+                          handleAddEvent(index);
+                        }}
+                      >
+                        캘린더 추가
+                      </button>
                       <Img src={item.awayTeamEmblemUrl} alt={item.category} />
                       <DetailContainer>
                         <Name>{item.league}</Name>
@@ -77,16 +171,14 @@ export default function Sports() {
 const SliderItem = styled.div`
   background-color: rgb(243, 250, 255);
   border-radius: 10px;
+  transition: transform 0.3s ease, filter 0.3s ease;
 
   img {
     height: 180px;
     object-fit: cover;
     margin-bottom: 10px;
-    transition: transform 0.3s ease;
+    transition: transform 0.3s ease, filter 0.3s ease;
     margin: 10px;
-    &:hover {
-      transform: scale(1.1);
-    }
   }
 `;
 
